280aa2f054cd28c3754230244439b9e2c04256fd
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-27 22:24:39 +0000

    fix linters errors

--- app/controllers/api/v1/greeting_controller.rb
+++ app/controllers/api/v1/greeting_controller.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 # This controller handles greetings in the API.
 class GreetingController < ApplicationController
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
-
+# frozen_string_literal: true
 
 # Ths is the application mailer.
 class ApplicationMailer < ActionMailer::Base
